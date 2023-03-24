24ffa43fd12845f2652b50effc2611ea883cc437
yousef <mr.ysaj@gmail.com>
2023-03-24 17:27:07 +0330

    [UPDATE] get_historical_data was updated

---
+++ coingecko.py
@@ -0,0 +1,35 @@
+from pycoingecko import CoinGeckoAPI
+import requests
+cg = CoinGeckoAPI()
+
+
+def get_price_token(token_name, pair):
+    """get price of a token or Coin at the moment"""
+    if not isinstance(token_name, dict):
+
+        return cg.get_price(ids=token_name, vs_currencies=pair)
+    else:
+        raise Exception("An Error was occur don't use Dict for token_list")
+
+
+fantom = get_price_token(token_name="fantom", pair="usd")
+
+
+def get_historical_data(id: str, days: str, interval: str, vs_currency: str):
+    """
+    id='bitcoin'
+    days= '150'
+    interval= 'daily'
+    vs_currency= 'usd'
+    """
+
+    try:
+        token_hist = cg.get_coin_market_chart_by_id(id=id, vs_currency=vs_currency, days=days, interval=interval)["prices"]
+        historical_list = list(map(lambda index: index[1], token_hist))
+        return historical_list
+    except Exception as e:
+        print(e)
+
+
+hist = get_historical_data(id="fantom", days='365', interval="daily", vs_currency="usd")
+print(hist)
